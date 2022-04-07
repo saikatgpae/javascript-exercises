f8f1ed1c5857d95d3cd4fa9bac87d1d7ca19ef38
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-07 20:48:04 +0530

    solved 05 with all constrains

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,40 @@
 const sumAll = function(a,b) {
     let sum = 0;
-    for(let i= a; i<=b; i++){
-        sum += i;
-        //console.log(sum+i);
+    if(a>=0 && b>=0 && typeof a == 'number' && typeof b == 'number'){
+        if(a<=b){
+            for(let i=a; i<=b; i++){
+                sum +=i;
+            }
+        }
+        else{
+            for(let i=b; i<=a; i++){
+                sum +=i;
+            }
+        }
     }
+    else{
+        return "ERROR"
+    }
+
+    
+    
     return sum;
 };
+
+/*
+if(a>b){
+        for(let i= a; i<=b; i++){
+            sum += i;
+            //console.log(sum+i);
+        }
+    }
+    else{
+        for(let i= b; i<=a; i++){
+            sum += i;
+    }
+    
+*/
+
 
 
 // Do not edit below this line
