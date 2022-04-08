cfce05dcea1e30dc659c441bf43292c27a0a8628
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-08 20:44:55 +0530

    solved problem upto 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  //[°C] = ([°F] − 32) × 5⁄9
+  C = (F - 32) * 5/9
+  if(C==0){
+    return parseInt(C.toFixed())
+  }else{
+    return parseFloat(C.toFixed(1))
+  }
+  
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+    //[°F] = [°C] × 9⁄5 + 32
+      F = C * 9/5 + 32
+      if(F == 0){
+        return parseInt(F.toFixed(1));
+      }else{
+        return parseFloat(F.toFixed(1));
+      }
+      
 };
-
+//[°C] = ([°F] − 32) × 5⁄9
+//[°F] = [°C] × 9⁄5 + 32
 // Do not edit below this line
 module.exports = {
   ftoc,
