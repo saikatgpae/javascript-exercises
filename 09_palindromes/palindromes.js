aa283567041f88832ca967f9d75ff32bb6ef9632
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-19 21:10:34 +0530

    solved till 08, 10, 09 skipped

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    reversedString = string.split('').reverse().join('');
+    if(string === reversedString){
+        return true
+    }else{
+        return false
+    }
 
 };
+/*
+var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
+if(format.test(string)){
+  return true;
+} else {
+  return false;
+}
+*/
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    var arr = [1,1];
+    var n = parseInt(x)
+    if(n<0){
+        return "OOPS";
+    }else{
+        for(let i=1; i<=n; i++){
+            let s = arr[i] + arr[i-1]
+            arr.push(s)
+        }
+    return arr[n-1];
+    }
 };
 
 // Do not edit below this line
