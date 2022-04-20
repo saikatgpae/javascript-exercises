1fc65775583fc39aefc0c64883789e7f0e2dde02
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-20 18:54:57 +0530

    solved till problem 11, problem 09 skipped

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    var booksTitle = [];
+    for(let i=0; i<books.length; i++){
+        booksTitle.push(books[i].title);
+    }
+    return booksTitle;
 };
 
 // Do not edit below this line
