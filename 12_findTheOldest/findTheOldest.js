d21f64948592f812575a7d9ac6e99139836e4f58
Saikat Chakraborty <saikatgpae@gmail.com>
2022-04-24 20:10:34 +0530

    solved till problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    const y = date.getFullYear();
+    const oldest = people.reduce((a, b) => {
+        if (a.yearOfDeath == undefined || b.yearOfDeath == undefined) {
+            const oldest = y - a.yearOfBirth;
+            const youngest = y - b.yearOfBirth;
+            return oldest > youngest ? a : b;
+        }
+        else{
+            const oldest = a.yearOfDeath - a.yearOfBirth;
+            const youngest = b.yearOfDeath - b.yearOfBirth;
+            return oldest > youngest ? a : b;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
